c10c2dcfaf58a6fa90fa22097fdc770e6f110863
GordonOtieno <otienogordon95@gmail.com>
2023-04-09 08:55:54 +0300

    Update project

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    //scope filters work with controller
+
+    public function scopeFilter($query, array $filters){
+        //dd($query);
+        if($filters['tag'] ?? false){
+            $query -> where('tags','like','%'.request('tag').'%');
+
+        }
+
+        if($filters['search'] ?? false){
+            $query -> where('title','like','%'.request('search').'%')
+            ->orWhere('description','like','%'.request('search').'%')
+            ->orWhere('tags','like','%'.request('search').'%');
+
+        }
+    }
 }
